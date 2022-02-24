afa52fa32de1e848f0b3c67d4a8b1eac7e024629
ImmortalLotus <72933183+ImmortalLotus@users.noreply.github.com>
2022-02-24 15:15:44 -0300

    fazendo testes manuais pra ver se a biblioteca estava funcionando

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Modelos;
+using System;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -7,6 +8,7 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ContaCorrente conta = new ContaCorrente(1, 1);
         }
     }
 }
